36be1dbd24d5dbd14c264613ce46f1de2c89795b
Zsolti <zsoltiberkes2@gmail.com>
2023-02-17 00:26:47 +0100

    profilescreen structure done

--- Thesis/app/screens/profilescreen/profilescreen.js
+++ Thesis/app/screens/profilescreen/profilescreen.js
@@ -4,7 +4,19 @@ import { View ,SafeAreaView, Text } from 'react-native'
 export default function ProfileScreen(){
     return(
         <SafeAreaView>
-            <Text>a</Text>
+             <Card style={loginScreenStyle.card} theme={{roundness:20,}}>
+                <Card.Title titleStyle={{ color:"rgba(0,153,218,200)", fontWeight:"bold", textAlign:"center"}} title="Saját adatok" ></Card.Title>
+                <Card.Content>
+                    <Image source={ Images.loginimage } style={loginScreenStyle.cardimage}/>
+                    <Text>Teljes név: {mockfullname}</Text>
+                    <Text>Neptun kód: {mockneptun}</Text>
+                    <Text>Kar és szak: </Text>
+                    <Text>Jelenlegi félév</Text>
+                    <Text>Felvett kurzusok: </Text>
+                    <Text>Teljesített sikeres tesztek: </Text>
+                    <Text>Sikertelen tesztek: </Text>
+                </Card.Content>
+            </Card>
         </SafeAreaView>
     )
 }

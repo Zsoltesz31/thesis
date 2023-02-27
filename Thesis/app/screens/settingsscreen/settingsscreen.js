7964d4e76cd02db4ec8814485994a6341d269e1f
Zsolti <zsoltiberkes2@gmail.com>
2023-02-27 13:53:32 +0100

    Auto stash before revert of "chooseLoginType screen added"

--- Thesis/app/screens/settingsscreen/settingsscreen.js
+++ Thesis/app/screens/settingsscreen/settingsscreen.js
@@ -1,21 +1,102 @@
 import React from 'react'
-import { View ,SafeAreaView, Text, Switch } from 'react-native'
+import { View ,SafeAreaView, Text, Switch, Touchable } from 'react-native'
 import {List} from 'react-native-paper'
 import { useState } from 'react'
 import {EventRegister} from 'react-native-event-listeners'
-import {Provider as PaperProvider} from 'react-native-paper'
 import {LanguageSelector} from './../../../components/languageSelector/index'
 import { useTranslation } from 'react-i18next'
+import { settingsscreenStyle } from './settingsscreenStyle'
+import {SectionList,Pressable } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 
+const settingsOptions = [
+    {
+        id:'1',
+        title:'Megjelenítés',
+        icon: ( <Ionicons name={'color-palette-outline'} size={10} color={'blue'}/> ),
+        data : [
+            'asd'
+        ]
+    },
+    {
+        id:'2',
+        title:'Nyelv',
+        icon: ( <Ionicons name={'language-outline'} size={10} color={'blue'}/> ),
+        data:[
+            'asd'
+        ] 
+    },
+
+    {
+        id:'3',
+        title:'Rólunk',
+        icon: ( <Ionicons name={'information-circle-outline'} size={10} color={'blue'}/> ),
+        data: [
+            'asd'
+        ]
+    },
+    {
+        id:'4',
+        title:'Értesítések',
+        icon: ( <Ionicons name={'notifications-outline'} size={10} color={'blue'}/> ),
+        data: [
+            'asd',
+            'asdasd'
+        ]
+    }
+]
 
 export default function ProfileScreen(){
     const [isEnabled, setIsEnabled] = useState(false);
     const {t} = useTranslation()
 
+    async function componentDidMount() {
+        await Font.loadAsync({
+          ionicons: Ionicons.font['ionicons'] 
+        });
+        this.setState({ isReady: true });
+      }
+
+        const Item = ({item}) => (
+        <Pressable onPress={()=>console.log(item.id)} android_ripple="true"> 
+        <View>
+            <Text>{item}</Text>
+        </View>
+        </Pressable>
+        )
+    
+  
+        const renderItem=({item,id}) =>{
+            console.log(id)
+            if(id==='2')
+                return(
+                    <LanguageSelector/>
+                ) 
+            else if(id==='1')
+                return(
+                    <Text>Teszt</Text>
+                )
+            else
+                return(
+                    <Item
+                    item={item}
+                    />
+                )
+            
+        }
+
+
     return(
-        <SafeAreaView>
-            <Text>Beállítások
-            </Text>
+        <SafeAreaView style={{flex:1}}>
+
+            <SectionList
+            sections = {settingsOptions}
+            keyExtractor={id=>id}
+            renderItem={renderItem}
+            renderSectionHeader={({section:{title,icon}}) =>(
+                <Text>{icon}{title}</Text>
+            )}
+            />
             <List.Section>
                 <List.Item title="Sötét mód" right={() => <Switch onValueChange={() => {setIsEnabled((value)=> !value)
                 EventRegister.emit("changeTheme",isEnabled)
---
+++ Thesis/app/screens/settingsscreen/settingsscreenStyle.js
@@ -0,0 +1,7 @@
+import {StyleSheet} from 'react-native'
+
+export const settingsscreenStyle = StyleSheet.create({
+    container: {
+      
+    }
+})

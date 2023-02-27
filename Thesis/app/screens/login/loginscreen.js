943dd783ac38b754747bbb5196339131e7ff4fa5
Zsolti <zsoltiberkes2@gmail.com>
2023-02-27 13:54:26 +0100

    loginScreen card Title changed to match userType

--- Thesis/app/screens/login/loginscreen.js
+++ Thesis/app/screens/login/loginscreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useState,useCallback } from 'react'
 import { SafeAreaView, View, Image } from 'react-native'
 import {Card, TextInput, Button} from 'react-native-paper'
 import { loginScreenStyle } from './loginscreenstyle'
@@ -6,15 +6,33 @@ import Images from '../../../images/index';
 import {CustomInput} from '../../../components/inputs/inputs'
 import {CustomButton} from '../../../components/buttons/buttons'
 import {theme} from '../../../AppStyle'
-import {Provider as PaperProvider} from 'react-native-paper'
-import { TouchableOpacity } from 'react-native-web';
 import { useDispatch } from 'react-redux';
 import { login } from "./../../../actions/auth"
+import { useFocusEffect } from '@react-navigation/native';
 
-export const LoginScreen=( { navigation }) =>{
+export default function LoginScreen({ route,navigation }) {
+
     const [username,setUsername] = useState('')
     const [password,setPassword] = useState('')
     const dispatch = useDispatch()
+    console.log(route.params.loginType)
+    const loginTitle = route.params.loginType === 'student' ? 'Bejelentkezés hallgatóknak' : 'Bejelentkezés tanároknak'
+    
+    /*useFocusEffect(()=>{
+        useCallback(()=>{
+        if(loginType==='student'){
+            setLoginTitle('Bejelentkezés hallgatóknak')
+            console.log(loginType)
+        }
+        else if(loginType==='teacher'){
+            setLoginTitle('Bejelentkezés tanároknak')
+            console.log(loginType)
+        }}
+        )
+        return() => {
+            console.log(loginType)
+        }
+    })*/
 
     const onLogin = () => {
         let user = {
@@ -32,17 +50,27 @@ export const LoginScreen=( { navigation }) =>{
         })
     }
 
+    const checkLoginType = () => {
+        if(loginType==='student'){
+            setLoginTitle('Bejelentkezés hallgatóknak')
+            console.log(loginType)
+        }
+        else if(loginType==='teacher'){
+            setLoginTitle('Bejelentkezés tanároknak')
+            console.log(loginType)
+        }
+    }
+
     return(
         <SafeAreaView style={loginScreenStyle.content} theme={theme}>
            <View style={loginScreenStyle.view}>
             <Card style={loginScreenStyle.card} theme={{roundness:20,}}>
-                <Card.Title titleStyle={{ color:"rgba(0,153,218,200)", fontWeight:"bold", textAlign:"center"}} title="Bejelentkezés" ></Card.Title>
+                <Card.Title titleStyle={{ color:"rgba(0,153,218,200)", fontWeight:"bold", textAlign:"center"}} title={loginTitle} ></Card.Title>
                 <Card.Content>
                     <Image source={ Images.loginimage } style={loginScreenStyle.cardimage}/>
                     <TextInput label="Neptun kód" mode='outlined' theme={{roundness:40}} value={username} onChangeText={text => setUsername(text)}></TextInput>
                     <TextInput label="Jelszó" mode ='outlined' secureTextEntry={true} theme={{roundness:40}} value={password} onChangeText={text => setPassword(text)}></TextInput>
                     <Button uppercase={false}>Elfelejtett jelszó</Button>
-
                     <CustomButton buttonName='Bejelentkezés' onPress={()=>onLogin()}></CustomButton>
                 </Card.Content>
             </Card>

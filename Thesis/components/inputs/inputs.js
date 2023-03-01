cc809b88b76017fa0cba09ed556773a55a61301d
Zsolti <zsoltiberkes2@gmail.com>
2023-03-01 13:39:26 +0100

    custom input, props added to help verification

--- Thesis/components/inputs/inputs.js
+++ Thesis/components/inputs/inputs.js
@@ -4,11 +4,11 @@ import { TextInput } from 'react-native-paper'
 import {inputsStyle} from './inputsstyle'
 
 
-export const CustomInput=({label,onChangeTextEvent,inputValue}) => {
+export const CustomInput=({label,onChangeTextEvent,inputValue,outlineColor}) => {
     return(
         
 
-        <TextInput value={inputValue} onChangeText={onChangeTextEvent} label={label} theme={{roundness:40}} outlineColor='#009AB9' style={{height:40, width:'70%',margin:5}} mode='outlined'
+        <TextInput value={inputValue} onChangeText={onChangeTextEvent} label={label} theme={{roundness:40}} outlineColor={outlineColor} style={{height:40, width:'70%',margin:5}} mode='outlined'
          >
          </TextInput>
 

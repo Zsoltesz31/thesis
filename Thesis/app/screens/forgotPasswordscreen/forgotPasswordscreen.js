aa50dd5947558125a6842e1814c97d67edcfa592
Zsolti <zsoltiberkes2@gmail.com>
2023-03-01 18:03:54 +0100

    validation done on ForgotPassword screen

--- Thesis/app/screens/forgotPasswordscreen/forgotPasswordscreen.js
+++ Thesis/app/screens/forgotPasswordscreen/forgotPasswordscreen.js
@@ -12,28 +12,32 @@ export default function ForgotPassword({route,navigation}){
     const [email,setEmail] = useState('')
     const [newPassword,setNewPassword] = useState('')
     const [newPassword2,setNewPassword2] = useState('')
-    const [errorMsg,setErrorMsg] = useState('')
-    const [pwErrorMsg,setPwErrorMsg] = useState('')
+    const [emailError,setEmailError] = useState('')
+    const [pswError, setPswError] = useState('')
+    
 
-    useEffect(()=>{
-        if(navigation.isFocused()){
-            console.log(errorMsg)
-        }
-    },[navigation.isFocused()])
+    
 
     const validateEmail = (email) =>{
         var re = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
         if(email==''){
             return (
-                setErrorMsg('Az e-mail mező nem lehet üresen!'),
-                setOutlineColor('red')
+                setEmailError('Az e-mail mező nem lehet üresen!'),
+                setOutlineColor('red'),
+                console.log('üres az email')
                 )
         }
         if(!re.test(email)){
             return(
-            setErrorMsg('A beírt e-mail nem megfelelő formátummal rendelkezik!'),
+             setEmailError('A beírt e-mail nem megfelelő formátummal rendelkezik!'),
             setOutlineColor('red'),
             console.log('Itt vagyok email nem jó formatum')
+            )
+        }
+        else{
+            return(
+                setOutlineColor('#009AB9'),
+                setEmailError('')
             )
         }
     }
@@ -42,57 +46,43 @@ export default function ForgotPassword({route,navigation}){
         if(password1.length <=0 || password2.length <=0)
         {
             return(
-            setPwErrorMsg('A jelszó mezők nem lehetnek üresen!'),
+            setPswError('A jelszó mezők nem lehetnek üresen!'),
             setPwOutlineColor('red'),
             console.log('üres a jelszo')
             )
         }
         if(password1!==password2){
             return(
-            setPwErrorMsg('A két jelszó nem egyezik meg!'),
+            setPswError('A két jelszó nem egyezik meg!'),
             setPwOutlineColor('red'),
             console.log('nem egyezik a két jelszó')
             )
         }
-    }
-
-    const validateOnSubmit = () => {
-        validateEmail(email)
-        validatePassword(newPassword,newPassword2)
-        if(errorMsg.length > 0 || pwErrorMsg.length >0){
-            console.log('Cant change passwords')
-        }
-        else
-        {
-        resetErrorStates()
-        console.log('Password changed')
+        else{
+            return(
+            setPwOutlineColor('#009AB9'),
+            setPswError('')
+            )
         }
     }
 
-    const resetErrorStates = () => {
-        setErrorMsg('')
-        setPwErrorMsg('')
-        setOutlineColor('#009AB9')
-        setPwOutlineColor('#009AB9')
-    }
-  
     return(
         <SafeAreaView style={ForgotPasswordscreenStyle.content}>
             <Text style={ForgotPasswordscreenStyle.title}>Jelszó emlékeztető</Text>
-            <CustomInput inputValue={email} onChangeTextEvent={text =>setEmail(text)} label={'E-mail'}  outlineColor={outlineColor}></CustomInput>
-            {errorMsg.length>0 && 
+            <CustomInput onEndEditing={validateEmail} inputValue={email} onChangeTextEvent={text =>setEmail(text)} label={'E-mail'}  outlineColor={outlineColor}></CustomInput>
+            {emailError.length>0 && 
             <Text>
-                {errorMsg}
+                {emailError}
             </Text>
             }
-            <CustomInput inputValue={newPassword} onChangeTextEvent={text=>setNewPassword(text)} label={'Új jelszó'} outlineColor={pwOutlineColor}></CustomInput>
-            <CustomInput inputValue={newPassword2} onChangeTextEvent={text=>setNewPassword2(text)} label={'Új jelszó újra'} outlineColor={pwOutlineColor}></CustomInput>
-            {pwErrorMsg.length>0 && 
+            <CustomInput  inputValue={newPassword} onChangeTextEvent={text=>setNewPassword(text)} label={'Új jelszó'} outlineColor={pwOutlineColor}></CustomInput>
+            <CustomInput  inputValue={newPassword2} onChangeTextEvent={text=>setNewPassword2(text)} label={'Új jelszó újra'} outlineColor={pwOutlineColor}></CustomInput>
+            {pswError.length>0 && 
             <Text>
-                {pwErrorMsg}
+                {pswError}
             </Text>
             }
-            <CustomButton buttonName={'Jelszó emlékeztető'} onPress={()=> validateOnSubmit()}></CustomButton>
+            <CustomButton buttonName={'Jelszó emlékeztető'} onPress={()=> {validateEmail(email);validatePassword(newPassword,newPassword2)}}></CustomButton>
             <CustomButton style={ForgotPasswordscreenStyle.backButton} buttonName={'Vissza'} onPress={()=>navigation.navigate('Login',{loginType:route.params.loginType})}></CustomButton>
         </SafeAreaView>
     )

773093aac2065a94171f658a8c2ddd6538712f6c
Zsolti <zsoltiberkes2@gmail.com>
2023-03-06 15:23:10 +0100

    CustomHeader added to courses screen

--- Thesis/app/screens/coursescreen/coursescreen.js
+++ Thesis/app/screens/coursescreen/coursescreen.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { SafeAreaView,Text } from 'react-native'
 import { Button} from 'react-native-paper'
 import ListView from '../../../components/listview/index'
+import CustomHeader from '../../../components/header/header'
 
-export default function CourseScreen({navigation}){
+export default function CourseScreen({route,navigation}){
     return(
         <SafeAreaView>
-           
+           <CustomHeader title={route.params.HeaderText}></CustomHeader>
             <Button mode="contained"  theme={{roundness:30}} onPress={()=> {navigation.navigate('Teszt létrehozása')}}>Tesztek</Button>
         </SafeAreaView>
     )

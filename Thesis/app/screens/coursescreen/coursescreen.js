053e31f3e3ee47817883894b51e01522ed99a19d
Zsolti <zsoltiberkes2@gmail.com>
2023-03-11 21:11:29 +0100

    courseLIst added to courses screen

--- Thesis/app/screens/coursescreen/coursescreen.js
+++ Thesis/app/screens/coursescreen/coursescreen.js
@@ -3,6 +3,7 @@ import { SafeAreaView,Text,View } from 'react-native'
 import { Button} from 'react-native-paper'
 import { coursescreenStyle } from './coursescreenStyle'
 import CustomHeader from '../../../components/header/header'
+import CourseList from '../../../components/courseList/courseList'
 
 export default function CourseScreen({route,navigation}){
     return(
@@ -10,6 +11,7 @@ export default function CourseScreen({route,navigation}){
            <CustomHeader title={route.params.HeaderText}></CustomHeader>
             <View style={coursescreenStyle.container}>
            <Text style={coursescreenStyle.listTitle}>Kurzusaid</Text>
+            <CourseList></CourseList>
             <Button mode="contained"  theme={{roundness:30}} onPress={()=> {navigation.navigate('Teszt létrehozása')}}>Tesztek</Button>
             </View>
         </SafeAreaView>

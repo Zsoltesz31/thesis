7da9c3fd693dd7fb5f02b86c373c2904ada8ed45
Zsolti <zsoltiberkes2@gmail.com>
2023-03-17 00:54:31 +0100

    testListScreen adde with style

---
+++ Thesis/app/screens/testlistscreen/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet,Dimensions } from "react-native";
+
+export const testListScreenStyle = StyleSheet.create({
+    icon:{
+        left:10,
+        marginRight:'auto'
+    },
+    title:{
+        color:'white',
+        textAlign:'center',
+        fontWeight:'bold',
+        fontSize:25,
+        right:Dimensions.get('window').width/2
+    },
+    titleContainer:{
+        display:'flex',
+        flexDirection:'row',
+        top:45
+    },
+    listContainer:{
+        top:150
+    },
+   
+})
--- Thesis/app/screens/testlistscreen/testlistscreen.js
+++ Thesis/app/screens/testlistscreen/testlistscreen.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { SafeAreaView,Text } from 'react-native'
+import { Pressable, SafeAreaView,Text,View } from 'react-native'
+import CustomHeader from '../../../components/header/header'
+import { testListScreenStyle } from './style'
+import { Ionicons } from '@expo/vector-icons';
+import TestList from '../../../components/testList/testList'
 
-export default function TestListScreen(){
+export default function TestListScreen({navigation,route}){
     return(
         <SafeAreaView>
-            <Text>Tesztek</Text>
+            <CustomHeader/>
+            <View style={testListScreenStyle.titleContainer}>
+                <Pressable style={testListScreenStyle.icon} onPress={()=>navigation.navigate('Kurzusaid')}><Ionicons name={'chevron-back-outline'} size={25} color={'white'}/></Pressable>
+                <Text style={testListScreenStyle.title}>{route.params.courseId}</Text>
+            </View>
+            <View style={testListScreenStyle.listContainer}>
+            <TestList courseId={route.params.courseId}></TestList>
+            </View>
         </SafeAreaView>
     )
 }

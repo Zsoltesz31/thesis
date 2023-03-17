6f442b6a59cc8cd3d18d1b9e3f53965bbfff24ad
Zsolti <zsoltiberkes2@gmail.com>
2023-03-17 20:52:35 +0100

    refactored and reconstructed modal component

--- Thesis/components/modals/confirmation_modal.js
+++ Thesis/components/modals/confirmation_modal.js
@@ -1,36 +1,27 @@
 import React, {useState} from "react"
 import {Alert, Modal, StyleSheet, Text, Pressable, View} from "react-native";
 import { styles } from "../modals/confirmation_modal_styles";
+import { Ionicons } from '@expo/vector-icons';
 
-export const ConfirmationModal =({stateChanger,modaltitle,handleclick,visible}) => {
+export const ConfirmationModal =({onClose,children,visible}) => {
 
 
     return(
-        <View style={styles.centeredView}>
-            <Modal
-            animationType="slide"
-            transparent={false}
-            visible={visible}
-            onRequestCLose={()=>{
-                Alert.alert("Modal has been closed")
-                setVisible(!visible)
-            }}
-            >
-                <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
-                        <Text style={styles.modalText}>{modaltitle}</Text>
-                        <Pressable style={[styles.button,styles.buttonClose]}
-                        onPress={()=> stateChanger(!visible)}>
-                            <Text style={styles.textStyle}>Igen</Text>
-                        </Pressable>
-                        <Pressable style={[styles.button,styles.buttonClose]}
-                         onPress={handleclick}>
-                            <Text style={styles.textStyle}>Nem</Text>
-                        </Pressable>
-                    </View>
+        <Modal
+        animationType="fade"
+        transparent={true}
+        visible={visible}
+        >
+        <View style={styles.modalContent}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>Teszt elkezdése</Text>
+                    <Pressable onPress={onClose}>
+                        <Ionicons name="close-circle-outline" color="#fff" size={22} />
+                    </Pressable>
                 </View>
-            </Modal>
+            {children}
         </View>
+        </Modal>
     )
 
 }
--- Thesis/components/modals/confirmation_modal_styles.js
+++ Thesis/components/modals/confirmation_modal_styles.js
@@ -1,45 +1,29 @@
 import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
-    centeredView: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 22
-    },
-    modalView: {
-      margin: 20,
-      backgroundColor: "white",
-      borderRadius: 20,
-      padding: 35,
-      alignItems: "center",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5
-    },
-    button: {
-      borderRadius: 20,
-      padding: 10,
-      elevation: 2
-    },
-    buttonOpen: {
-      backgroundColor: "#F194FF",
-    },
-    buttonClose: {
-      backgroundColor: "#2196F3",
-    },
-    textStyle: {
-      color: "white",
-      fontWeight: "bold",
-      textAlign: "center"
-    },
-    modalText: {
-      marginBottom: 15,
-      textAlign: "center"
-    }
+  modalContent: {
+    height: '30%',
+    width: '95%',
+    backgroundColor: 'white',
+    borderRadius:18,
+    position: 'absolute',
+    bottom: '40%',
+    left:'2.5%',
+    elevation:5
+  },
+  titleContainer: {
+    height: '16%',
+    backgroundColor: '#009AB9',
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight:'bold'
+  },
   });

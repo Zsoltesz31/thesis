31cb8247cd3bf213759eaa8fcd3f73bc250917ef
Zsolti <zsoltiberkes2@gmail.com>
2023-03-17 20:52:23 +0100

    added icons to TestList screen to check list completion statius

--- Thesis/components/testList/testList.js
+++ Thesis/components/testList/testList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {View,Text,SafeAreaView,FlatList,Pressable } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 import {testListStyle} from'./style'
 
 
@@ -8,31 +9,36 @@ const data = [
         id:1,
         courseId:1,
         deadline:'10.16 00:00',
-        title:'Programmozás módszertan'
+        title:'Programmozás módszertan',
+        succes:null
     },
     {
         id:2,
         courseId:1,
         deadline:'10.16 00:00',
-        title:'Magasszintű programmozási nyelvek'
+        title:'Magasszintű programmozási nyelvek',
+        succes:false
     },
     {
         id:3,
         courseId:2,
         deadline:'10.16 00:00',
-        title:'Webprogrammozás 1'
+        title:'Webprogrammozás 1',
+        succes:true
     },
     {
         id:4,
         courseId:2,
         deadline:'10.16 00:00',
-        title:'Webprogrammozás 1'
+        title:'Webprogrammozás 1',
+        succes:true
     },
     {
         id:5,
         courseId:3,
         deadline:'10.16 00:00',
-        title:'Webprogrammozás 1'
+        title:'Webprogrammozás 1',
+        succes:true
     }
 ]
 
@@ -43,10 +49,22 @@ export default function TestList({navigation,courseId}){
     
 
     const Item = ({item}) => (
-        <Pressable onPress={()=>console.log(courseId)} android_ripple="true"> 
-        <View style={testListStyle.listitem}>
-        <Text style={testListStyle.listItemHeader}>{item.title}</Text> 
+        <Pressable onPress={()=>navigation.navigate('TestSheet',{testId:item.id,testName:item.title,courseId:courseId})} android_ripple="true"> 
+        <View style={[testListStyle.listitem, item.succes == null ? testListStyle.defaultBorder : (item.succes && true ? testListStyle.succesBorder : testListStyle.failBorder)]}>
+        <Text style={testListStyle.listItemHeader}>{item.title}</Text>
+        <View style={testListStyle.listCrudButtons}>
+        <Pressable><Ionicons name={'create-outline'} size={20} color={"white"}/></Pressable>
+        <Pressable><Ionicons name={'trash-outline'} size={20} color={"white"}/></Pressable>
+        </View>
+        <View style={testListStyle.testFooterContainer}>
+        {item.succes == true &&
+              <Ionicons style={testListStyle.icon} name={'checkmark-outline'} size={20} color={"white"}/>
+        }
+         {item.succes == false &&
+              <Ionicons style={testListStyle.icon} name={'close-outline'} size={20} color={"white"}/>
+        }
         <Text style={testListStyle.deadline}>Határidő: {item.deadline}</Text>
+        </View>
         </View>
         </Pressable>
     )
@@ -61,6 +79,7 @@ export default function TestList({navigation,courseId}){
     return(
           <SafeAreaView style={testListStyle.container}>
             <FlatList
+            showsHorizontalScrollIndicator={false}
             data={data.filter(item=> item.courseId==courseId)}
             renderItem={renderItem}
             keyExtractor = {item=>item.id.toString()}

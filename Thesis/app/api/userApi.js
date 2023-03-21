30446255e44885ab4092ac5eecfec744d3019f7a
Zsolti <zsoltiberkes2@gmail.com>
2023-03-21 23:37:34 +0100

    userApi path to register added

--- Thesis/app/api/userApi.js
+++ Thesis/app/api/userApi.js
@@ -22,3 +22,25 @@ export const userLogin = async (data) => {
         }
     }
 }
+
+export const userRegister = async (data) =>{
+    try {
+        const result = await BaseApi('/auth/signup',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            data:data
+        })
+        return result
+    }  catch (error) {
+        if(error.response){
+           console.log(error.response.data)
+        } else if(error.request) {
+            console.log(error.request)
+        } else{
+            console.log('Error',error.message)
+        }
+        
+    }
+}

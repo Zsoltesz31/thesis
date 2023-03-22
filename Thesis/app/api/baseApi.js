5bee6827ebfb8429dbb5e83788eaab624d3ee778
Zsolti <zsoltiberkes2@gmail.com>
2023-03-22 14:55:54 +0100

    removed unused content

--- Thesis/app/api/baseApi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const BaseApi = axios.create({
-    baseURL:'http://192.168.1.66:3333',
-    responseType:'json',
-    withCredentials:true
-})
-
-export default BaseApi

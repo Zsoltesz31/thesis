354d3f3b4f154a55b7e5b422b5cba5df1fc58e4e
Zsolti <zsoltiberkes2@gmail.com>
2023-03-22 14:57:04 +0100

    AuthProveder added to app.js

--- Thesis/App.js
+++ Thesis/App.js
@@ -3,14 +3,17 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Navigations from './navigation/navigation.js'
 import './translations/i18config'
 import { Provider as StoreProvider} from "react-redux"
+import { AuthProvider } from './context/AuthContext.js';
 import store from "./store"
 
 export default function App() {
  
   return (
     <StoreProvider store={store}>
-  <Navigations/>
-  </StoreProvider>
+    <AuthProvider>
+    <Navigations/>
+    </AuthProvider>
+    </StoreProvider>
   )
 }
 

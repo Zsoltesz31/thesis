ceb5d9b5f64e251081b8683ad878270c7f9b6a0b
Zsolti <zsoltiberkes2@gmail.com>
2023-03-24 22:22:56 +0100

    createTestScreen edit mode and add mode added

--- Thesis/app/screens/createTestScreen/createTestScreen.js
+++ Thesis/app/screens/createTestScreen/createTestScreen.js
@@ -1,29 +1,47 @@
-import React, {useState,useContext} from 'react'
-import { useDispatch } from 'react-redux'
+import React, {useState,useContext,useEffect} from 'react'
+import { useDispatch,useSelector } from 'react-redux'
 import { SafeAreaView,View,Text,Pressable} from 'react-native'
 import CustomHeader from '../../../components/header/header'
 import { CustomButton } from '../../../components/buttons/buttons'
 import { CustomInput } from '../../../components/inputs/inputs'
 import { CreateTestScreenStyle } from './style'
 import { Ionicons } from '@expo/vector-icons';
-import { createTest } from '../../../slices/testSlice'
+import { createTest,getAllTests,getTestById,updateTest } from '../../../slices/testSlice'
 import { AuthContext } from '../../../context/AuthContext'
 
-export default function CreateTestScreen({navigation}){
+export default function CreateTestScreen({route,navigation}){
     const [title,setTitle] = useState('')
     const [description,setDescription] = useState('')
     const dispatch = useDispatch()
     const {userData} = useContext(AuthContext)
+    const editMode = route.params.edit
 
-    const handleAdd =()=>{
+
+    const handleAdd = ()=>{
         let values = {
             title:title,
             description:description,
             ownerId:userData.id
         }
-        dispatch(createTest(values))
+         dispatch(createTest(values))
+        dispatch(getAllTests())
+        navigation.navigate('Tesztek')
     }
 
+    const handleEdit = () =>{
+        let values = {
+            title:title,
+            description:description,
+            ownerId:userData.id,
+            testId: route.params.testData.test.id
+        }
+         dispatch(updateTest(values))
+        dispatch(getAllTests())
+        navigation.navigate('Tesztek')
+    }
+
+    if(editMode==false)
+    {
     return(
         <SafeAreaView>
             <CustomHeader/>
@@ -39,4 +57,23 @@ export default function CreateTestScreen({navigation}){
             </View>
         </SafeAreaView>
     )
+    }else if(editMode==true){
+        const [title,setTitle] = useState(route.params.testData.test.title)
+         const [description,setDescription] = useState(route.params.testData.test.description)
+        return(
+        <SafeAreaView>
+            <CustomHeader/>
+            <View style={CreateTestScreenStyle.titleContainer}>
+                <Pressable style={CreateTestScreenStyle.icon} onPress={()=>navigation.navigate('Kurzusaid')}><Ionicons name={'chevron-back-outline'} size={25} color={'white'}/></Pressable>
+                <Text style={CreateTestScreenStyle.title1}>Teszt módosítása</Text>
+            </View>
+            <View style={CreateTestScreenStyle.formContainer}>
+                <Text style={CreateTestScreenStyle.formTitle}>Teszt adatai</Text>
+                <CustomInput inputValue={title} label={'Teszt neve'} onChangeTextEvent={text => setTitle(text)} outlineColor={'#009AB9'}></CustomInput>
+                <CustomInput inputValue={description} label={'Teszt leírása'} onChangeTextEvent={text => setDescription(text)} outlineColor={'#009AB9'}></CustomInput>
+                <CustomButton buttonName={'Módosít'} onPress={()=>handleEdit()}></CustomButton>
+            </View>
+        </SafeAreaView>
+        )
+    }
 }

e8f15a7a6a4fa3ac209629236c948a3858c96ad1
Zsolti <zsoltiberkes2@gmail.com>
2023-03-27 19:14:00 +0200

    added multiline option to custom input component

--- Thesis/components/inputs/inputs.js
+++ Thesis/components/inputs/inputs.js
@@ -4,11 +4,11 @@ import { TextInput } from 'react-native-paper'
 import {inputsStyle} from './inputsstyle'
 
 
-export const CustomInput=({label,onChangeTextEvent,inputValue,outlineColor,onBlurEvent}) => {
+export const CustomInput=({label,onChangeTextEvent,inputValue,outlineColor,onBlurEvent,multiline}) => {
     return(
         
 
-        <TextInput onBlur={onBlurEvent} value={inputValue} onChangeText={onChangeTextEvent} label={label} theme={{roundness:40}} outlineColor={outlineColor} style={{height:40, margin:5}} mode='outlined'
+        <TextInput multiline={multiline} onBlur={onBlurEvent} value={inputValue} onChangeText={onChangeTextEvent} label={label} theme={{roundness:40}} outlineColor={outlineColor} style={{height:40, margin:5}} mode='outlined'
          >
          </TextInput>
 

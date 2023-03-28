a462cfe7b9d77ded7cfd7b89176977bd035ee147
Zsolti <zsoltiberkes2@gmail.com>
2023-03-28 16:03:46 +0200

    shortAnwserQuestion component funcitonality done with styling

--- Thesis/components/questionComponents/shortAnswerQuestion/style.js
+++ Thesis/components/questionComponents/shortAnswerQuestion/style.js
@@ -1,5 +1,18 @@
-import { StyleSheet } from "react-native-web";
+import { StyleSheet } from "react-native";
 
 export const ShortAnswerQuestionStyle = StyleSheet.create({
-    
+    questionTitle:{
+        fontSize:18,
+        fontWeight:'bold',
+        textAlign:'center',
+        color:'#009AB9'
+    },
+    questionContainer:{
+        elevation:2,
+        height:150,
+        justifyContent:'center',
+        borderRadius:5,
+        width:'95%',
+        alignSelf:'center'
+    }
 })

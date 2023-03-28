41a0240438f13f9f4f7a43875ff14a61db2f3da5
Zsolti <zsoltiberkes2@gmail.com>
2023-03-28 16:10:41 +0200

    longAnwserQuestion component funcitonality done with styling

--- Thesis/components/questionComponents/longAnswerQuestion/style.js
+++ Thesis/components/questionComponents/longAnswerQuestion/style.js
@@ -1,5 +1,19 @@
-import { StyleSheet } from "react-native-web";
+import { StyleSheet } from "react-native";
+
 
 export const LongAnswerQuestionStyle = StyleSheet.create({
-    
+    questionTitle:{
+        fontSize:18,
+        fontWeight:'bold',
+        textAlign:'center',
+        color:'#009AB9'
+    },
+    questionContainer:{
+        elevation:2,
+        height:150,
+        justifyContent:'center',
+        borderRadius:5,
+        width:'95%',
+        alignSelf:'center'
+    }
 })

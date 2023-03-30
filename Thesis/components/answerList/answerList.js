b8b45f4e33dae3cac4e52d54a44eb514fe6709b3
Zsolti <zsoltiberkes2@gmail.com>
2023-03-30 22:19:14 +0200

    added Edit functionality to answerList

--- Thesis/components/answerList/answerList.js
+++ Thesis/components/answerList/answerList.js
@@ -1,47 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import {View,Text,SafeAreaView,FlatList,Pressable } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 import {answerListStyle} from'./style'
 import { useSelector,useDispatch } from 'react-redux'
-import { fetchPosts } from '../../slices/courseSlice';
+import { deleteAnswer,updateAnswer } from '../../slices/answerSlice';
+import { ConfirmationModal } from '../modals/confirmation_modal';
+import { CustomInput } from '../inputs/inputs';
+import { CustomButton } from '../buttons/buttons';
+import { Checkbox } from 'react-native-paper';
+import { ModalInputsComponent } from '../modalInputsComponent/modalInputsComponent';
 
 
-const data = [
-    {
-        id:1,
-        title:'Első válasz',
-        correct:true
-    },
-    {
-        id:2,
-        title:'Második válasz',
-        correct:false
-    },
-    {
-        id:3,
-        title:'Harmadik válasz',
-        correct:false
-    },
-    {
-        id:4,
-        title:'Harmadik válasz',
-        correct:false
-    },
-  
-]
+export default function AnswerList({navigation,data,changeHappened,questionId}){
+    const dispatch=useDispatch()
+    const [isModalVisible,setIsModalVisible] = useState(false)
+    const {currentAddedQuestion} =useSelector((state)=>state.question)
+
+    const handleDelete = (id) =>{
+        dispatch(deleteAnswer(id)).then(changeHappened(true))
+    }
+
+    const handleEdit = (id,text,correct) =>{
+        let values = {
+            questionId:currentAddedQuestion.data.id,
+            id:id,
+            text:text,
+            correct:correct
+        }
+        dispatch(updateAnswer(values))
+        modalClose()
+    }
+
+    const modalClose = () =>{
+        setIsModalVisible(false)
+    }
 
 
-
-
-export default function AnswerList({navigation}){
 
     const Item = ({item}) => (
         <Pressable android_ripple="true"> 
         <View style={answerListStyle.listitem}>
-        <Text style={answerListStyle.listItemHeader}>{item.title}</Text> 
-        <Ionicons  name={item.correct ? 'checkmark-outline' : 'close-outline'} size={20} color={"white"}/>
-        <Pressable style={answerListStyle.buttonTest}><Ionicons  name={'create-outline'} size={20} color={"white"}/></Pressable>
-        <Pressable style={answerListStyle.buttonTest}><Ionicons  name={'trash-outline'} size={20} color={"white"}/></Pressable>
+        <Text style={answerListStyle.listItemHeader}>{item.text}</Text> 
+        <Ionicons  name={item.correct ? 'checkmark-outline' : 'close-outline'} size={20} color={item.correct ? 'green':'red'}/>
+        <Pressable onPress={()=>handleDelete(item.id)} style={answerListStyle.buttonTest}><Ionicons  name={'trash-outline'} size={20} color={"white"}/></Pressable>
+        <Pressable onPress={()=>setIsModalVisible(true)} style={answerListStyle.buttonTest}><Ionicons  name={'create-outline'} size={20} color={"white"}/></Pressable>
+        <ConfirmationModal visible={isModalVisible} onClose={modalClose}>
+        <ModalInputsComponent idToUpdate={item.id} handleFunction={handleEdit} answertext={item.text} checkedstatus={item.correct} ></ModalInputsComponent>
+        </ConfirmationModal>
         </View>
         </Pressable>
     )
@@ -55,7 +60,6 @@ export default function AnswerList({navigation}){
     }
 
 
-
     if(data==[])
     {
     return(
@@ -66,10 +70,9 @@ export default function AnswerList({navigation}){
     return(
           <SafeAreaView style={answerListStyle.container}>
             <FlatList
-            data={data}
+            data={data.data}
             renderItem={renderItem}
-            keyExtractor = {item=>item.id.toString()}
-          
+            keyExtractor={item=>item.id.toString()}
             ></FlatList>
           </SafeAreaView>  
         

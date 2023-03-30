28c3dca6b259cc6711a6b678ea912f9cc2d11c43
Zsolti <zsoltiberkes2@gmail.com>
2023-03-30 22:17:15 +0200

    questionList component added with style

---
+++ Thesis/components/questionList/questionList.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import {View,Text,SafeAreaView,FlatList,Pressable } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import { questionListStyle } from './style';
+import { useSelector,useDispatch } from 'react-redux'
+import { deleteQuestion } from '../../slices/questionSlice';
+
+
+export default function QuestionList({navigation,data,changeListener,testId,testName}){
+    const dispatch=useDispatch()
+
+    const handleDelete = (id) =>{
+        dispatch(deleteQuestion(id)).then(changeListener(true))
+    }
+
+
+    const Item = ({item}) => (
+        <Pressable android_ripple="true"> 
+        <View style={questionListStyle.listitem}>
+        <Text style={questionListStyle.listItemHeader}>{item.text}</Text> 
+        <Text style={questionListStyle.listItemHeader}>{item.type}</Text> 
+        <Pressable onPress={()=>handleDelete(item.id)} style={questionListStyle.buttonTest}><Ionicons  name={'trash-outline'} size={20} color={"white"}/></Pressable>
+        <Pressable onPress={()=>navigation.navigate("AddQuestionWithAnswer",{qestionText:item.text,questionType:item.type,FullEditMode:true,questionId:item.id,testId:testId,testName:testName})} style={questionListStyle.buttonTest}><Ionicons  name={'create-outline'} size={20} color={"white"}/></Pressable>
+        </View>
+        </Pressable>
+    )
+    
+    const renderItem=({item}) =>{
+        return(
+            <Item
+            item={item}
+            />
+        )
+    }
+
+
+    if(data==[])
+    {
+    return(
+        <Text>Nincs hozzáadott kérdés!</Text>
+    )
+    }
+    else{
+    return(
+          <SafeAreaView style={questionListStyle.container}>
+            <FlatList
+            data={data.data}
+            renderItem={renderItem}
+            keyExtractor={item=>item.id.toString()}
+            ></FlatList>
+          </SafeAreaView>  
+        
+    )
+    }
+}

dd7a7260cb9c5b9e7f1b3abde7fde941c5a15b67
Zsolti <zsoltiberkes2@gmail.com>
2023-03-30 22:17:04 +0200

    modalInputsCOmponent added with style
    separate logic to stop re-rendering

---
+++ Thesis/components/modalInputsComponent/modalInputsComponent.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react"
+import {Alert, Modal, StyleSheet, Text, Pressable, View} from "react-native";
+import { ModalInputsComponentStyle } from "./style";
+import { Ionicons } from '@expo/vector-icons';
+import { CustomInput } from "../inputs/inputs";
+import { CustomButton } from "../buttons/buttons";
+import { Checkbox } from "react-native-paper";
+
+export const ModalInputsComponent = ({idToUpdate,answertext,checkedstatus,handleFunction}) => {
+    const [checked,setChecked] =useState(checkedstatus)
+    const [answer,setAnswer] = useState(answertext)
+    return(
+        <View style={ModalInputsComponentStyle.modalContent}>
+        <Text style={ModalInputsComponentStyle.modalTitle}>Válasz módosítása</Text>
+                <CustomInput inputValue={answer} label={'Válasz'} outlineColor={'#009AB9'} onChangeTextEvent={text => setAnswer(text)}></CustomInput>
+                <View style={ModalInputsComponentStyle.checkBoxContainer}>
+                <Text style={ModalInputsComponentStyle.checkBoxText}>Helyes válasz:</Text>
+                <Checkbox status={checked ? 'checked' : 'unchecked'} onPress={()=>{setChecked(!checked)}} color={'#009AB9'} uncheckedColor={'#009AB9'}></Checkbox>
+                </View>
+                <CustomButton buttonName={'Válasz módosítása'} onPress={()=>handleFunction(idToUpdate,answer,checked)}></CustomButton>
+        </View>
+    )
+}
---
+++ Thesis/components/modalInputsComponent/style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native"
+
+export const ModalInputsComponentStyle=StyleSheet.create({
+modalContent:{
+    justifyContent:'center'
+  },
+modalTitle:{
+    fontWeight:'bold',
+    fontSize:15,
+    textAlign:'center'
+  },
+  checkBoxContainer:{
+    flexDirection:'row',
+    width:'100%',
+    justifyContent:'center'
+},
+checkBoxText:{
+    textAlign:'center',
+    fontWeight:'bold',
+    fontSize:18,
+    color: '#009AB9',
+    alignSelf:'center',
+    bottom:2
+}
+})

b11be5f03fa684ea10c52d85a2198b67c53e7fc0
Zsolti <zsoltiberkes2@gmail.com>
2023-04-03 21:16:33 +0200

    button added to check published tests

--- Thesis/app/screens/testlistscreen/testlistscreen.js
+++ Thesis/app/screens/testlistscreen/testlistscreen.js
@@ -6,17 +6,24 @@ import { testListScreenStyle } from './style'
 import { Ionicons } from '@expo/vector-icons';
 import {useSelector,useDispatch} from 'react-redux'
 import { getAllTests } from '../../../slices/testSlice'
+import { getUpcomingTestByUserId } from '../../../slices/upcommingTestSlice'
 import TestList from '../../../components/testList/testList'
 
 
 export default function TestListScreen({navigation,route}){
     const dispatch = useDispatch()
     const {tests} =useSelector((state)=>state.test)
+    const {upComingTests} =useSelector((state)=>state.upComingTest)
     const [changeHappened,setChangeHappened] = useState(false)
 
-
     useEffect(()=>{
+        if(route.params.testListMode=='Tests'){
         dispatch(getAllTests())
+        }
+        else if(route.params.testListMode=='upComingTests')
+        {
+            dispatch(getUpcomingTestByUserId(1))
+        }
         setChangeHappened(false)
     },[changeHappened])
 
@@ -31,10 +38,10 @@ export default function TestListScreen({navigation,route}){
             <CustomHeader/>
             <View style={testListScreenStyle.titleContainer}>
                 <Pressable style={testListScreenStyle.icon} onPress={()=>navigation.navigate('Kurzusaid')}><Ionicons name={'chevron-back-outline'} size={25} color={'white'}/></Pressable>
-                <Pressable><Text style={testListScreenStyle.title}>ABCD</Text></Pressable>
+                <Text style={testListScreenStyle.title}>Tesztek</Text>
             </View>
             <View style={testListScreenStyle.listContainer}>
-            <TestList changeListener={changeTracker} data={tests} navigation={navigation}></TestList>
+            <TestList changeListener={changeTracker} data={route.params.testListMode=='Tests'? tests : upComingTests} navigation={navigation} listType={route.params.testListMode}></TestList>
             <CustomButton buttonName={'Teszt létrehozása'} onPress={()=>navigation.navigate('CreateTest',{edit:false,testData:{title:'',description:''}})}></CustomButton>
             </View>
         </SafeAreaView>
